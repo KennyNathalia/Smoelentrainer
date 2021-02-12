ff37169dfb631fb70742b95106bd4a5d62ea4164
Kenny Nathalia <99057679@mydavinci.nl>
2021-02-12 10:40:46 +0100

    Changes

---
+++ players.js
@@ -0,0 +1,64 @@
+var players = [{
+	"name": "bart",
+	"img": "bart.jpg"
+},
+
+{
+	"name": "christian",
+	"img": "christian.jpg"
+},
+
+{
+	"name": "eric",
+	"img": "eric.jpg"
+},
+
+{
+	"name": "jens",
+	"img": "jens.jpg"
+},
+
+{
+	"name": "justin",
+	"img": "justin.jpg"
+},
+
+{
+	"name": "mark",
+	"img": "mark.jpg"
+},
+
+{
+	"name": "marcos",
+	"img": "marcos.jpg"
+},
+
+{
+	"name": "nicolai",
+	"img": "nicolai.jpg"
+},
+
+{
+	"name": "steven",
+	"img": "steven.jpg"
+},
+
+{
+	"name": "tyrell",
+	"img": "tyrell.jpg"
+},
+
+{
+	"name": "uros",
+	"img": "uros.jpg"
+},
+];
+
+for(var i = 0; i< players.length; i++){
+	players[i]	
+
+	console.log(players[i]);
+}
+
+
+console.log(players);
---
+++ smoelentrainer.js
@@ -0,0 +1,3 @@
+document.getElementById('spelers').onclick = function(){
+    window.location.href="players.html";
+}
