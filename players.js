1bb3608e8d2ffe6a492ff5fead469f7dc4a5280e
Kenny Nathalia <99057679@mydavinci.nl>
2021-02-17 14:07:23 +0100

    Buttons randomized, timer, classes hernoemd, match functie

--- players.js
+++ players.js
@@ -1,89 +1,96 @@
 var players = [{
-	"name": "bart",
+	"name": "Bart",
 	"img": "img/bart.jpg"
 },
 
 {
-	"name": "christian",
+	"name": "Christian",
 	"img": "img/christian.jpg"
 },
 
 {
-	"name": "eric",
+	"name": "Eric",
 	"img": "img/eric.jpg"
 },
 
 {
-	"name": "jens",
+	"name": "Jens",
 	"img": "img/jens.jpg"
 },
 
 {
-	"name": "justin",
+	"name": "Justin",
 	"img": "img/justin.jpg"
 },
 
 {
-	"name": "mark",
+	"name": "Mark",
 	"img": "img/mark.jpg"
 },
 
 {
-	"name": "marcos",
+	"name": "Marcos",
 	"img": "img/marcos.jpg"
 },
 
 {
-	"name": "nicolai",
+	"name": "Nicolai",
 	"img": "img/nicolai.jpg"
 },
 
 {
-	"name": "steven",
+	"name": "Steven",
 	"img": "img/steven.jpg"
 },
 
 {
-	"name": "tyrell",
+	"name": "Tyrell",
 	"img": "img/tyrell.jpg"
 },
 
 {
-	"name": "uros",
+	"name": "Uros",
 	"img": "img/uros.jpg"
 },
 ];
 
-var selectedImgId;
+var selectedImg;
 var selectedName;
-
+var checkImg;
+var checkName;
+var count = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const player = document.querySelectorAll(".players");
-const namePlayer = document.querySelectorAll(".namePlayer")
+const namePlayer = document.querySelectorAll(".namePlayer");
+
 
 player.forEach(player => {
 	player.onclick = function(){ selectIMG(player.id); }
-	console.log(player.id);
+});
+
+namePlayer.forEach(namePlayer => {
+	namePlayer.onclick = function(){ selectName(namePlayer.id); }
 });
 
 //gives border to the images
 function selectIMG(id){
-	if (selectedImgId == undefined) {
+	if (selectedImg == undefined) {
 		document.getElementById(id).classList.add("borders");
-		selectedImgId = id;
+		selectedImg = id;
+		checkImg = document.getElementById(id).src;
 	}else{
-		document.getElementById(selectedImgId).classList.remove("borders");
+		document.getElementById(selectedImg).classList.remove("borders");
 		document.getElementById(id).classList.add("borders");
-		selectedImgId = id;
+		selectedImg = id;
+		checkImg = document.getElementById(id).src;
 	}
+	checkImg = checkImg.substring(checkImg.indexOf("img"));
+	console.log(checkImg);
+	checkMatch();
 }
 
-namePlayer.forEach(namePlayer => {
-	namePlayer.onclick = function(){ selectIMG(namePlayer.id); }
-	console.log(namePlayer.id);
-});
 
 //gives border to the names
-function selectedName(id){
+function selectName(id){
 	if (selectedName == undefined) {
 		document.getElementById(id).classList.add("bordersName");
 		selectedName = id;
@@ -92,17 +99,54 @@ function selectedName(id){
 		document.getElementById(id).classList.add("bordersName");
 		selectedName = id;
 	}
+	checkName = document.getElementById(id).innerHTML;
+	console.log(checkName);
+	checkMatch();
 }
 
-var count = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+//Randomizes the position of the players and names
 for (var i=0; i<players.length; i++){
 	var random = count[Math.floor(Math.random() * count.length)];
 	var countCheck = count.indexOf(random);
 	count.splice(countCheck, 1);
 
 	document.getElementById("img-"+i).src= players[random].img;
+	document.getElementById("btn-"+i).innerHTML= players[random].name;
 }
 
- 
 
+//checks if image matches with the name or if the name matches with the image
+function checkMatch(){
+	var checkNoMatch;
+
+	if (checkImg != undefined && checkName != undefined) {
+		for (var i=0; i < players.length; i++){
+			if(players[i].img === checkImg && players[i].name === checkName){
+				console.log("yea");
+				checkNoMatch = false;
+			}else{
+				console.log("nah");
+				checkNoMatch = true;
+			}
+		}
+	}
+	if (checkNoMatch = true){
+		alert("test");
+	}
+}
+
+
+//2 min timer
+var timeleft = 120;
+var width = 100;
+var barAmount = Number(width/timeleft);
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+  }
+  width = width - barAmount;
+  document.getElementById("progressBar").value = 120 - timeleft;
+  document.getElementById("timerBar").style.width = width + "%";
+  timeleft -= 1;
+}, 1000);
