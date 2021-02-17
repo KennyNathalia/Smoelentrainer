7d787aee286e9ceea7ee98bf5f62a514dfe1f71a
Kenny Nathalia <99057679@mydavinci.nl>
2021-02-17 14:06:29 +0100

    Thema veranderen
    Je kan kiezen tussen 3 kleuren

--- smoelentrainer.js
+++ smoelentrainer.js
@@ -1,3 +1,32 @@
-document.getElementById('spelers').onclick = function(){
-    window.location.href="players.html";
+const green = document.querySelector('.green');
+
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
+
+green.onclick = function() {
+  const rndCol = '#008000';
+  document.body.style.backgroundColor = rndCol;
+}
+
+const red = document.querySelector('.red');
+
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
+
+red.onclick = function() {
+  const rndCol = '#FF0000';
+  document.body.style.backgroundColor = rndCol;
+}
+
+const blue = document.querySelector('.blue');
+
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
+
+blue.onclick = function() {
+  const rndCol = '#00BFFF';
+  document.body.style.backgroundColor = rndCol;
 }
